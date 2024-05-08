e29c9466d0d5d3eb2ad6054c14966ef20bf4adda
pseudoc <atlas.yu@canonical.com>
2024-05-08 09:55:22 +0800

    calc: create an `add` fn
    Since we just got started, refactoring won't tak too much efforts.

--- calc.py
+++ calc.py
@@ -1,18 +1,22 @@
+def add(x, y):
+    return x + y
+add.symbol = "+"
+
 def calc_args():
     return {
         "left": 1,
         "right": 2,
-        "operator": ("+", lambda x, y: x + y)
+        "operator": add,
     }
 
 def calc():
     args = calc_args()
     left = args["left"]
     right = args["right"]
-    op_name, op_fn = args["operator"]
+    op_fn = args["operator"]
 
     result = op_fn(left, right)
-    print(f"{left} {op_name} {right} = {result}")
+    print(f"{left} {op_fn.symbol} {right} = {result}")
 
 if __name__ == "__main__":
     calc()
